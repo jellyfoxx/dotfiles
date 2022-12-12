be3dd4c1511a9309c963b2cccca77647217c3b5e
jellyfox <114638090+jellyfoxx@users.noreply.github.com>
2022-12-12 22:51:49 +0000

    bspwm commit

---
+++ config/bspwm/startup_apps.sh
@@ -0,0 +1,11 @@
+sxhkd &
+/home/eve/.config/polybar/launch-bspwm.sh &
+/home/eve/.fehbg &
+picom --experimental-backends &
+blueman-applet &
+blueman-tray &
+alsactl restore &
+exec flameshot &
+discord --start-minimized &
+steam -silent &
+telegram-desktop -startintray &
